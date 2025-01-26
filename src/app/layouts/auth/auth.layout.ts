dfbe0a86876aae4e7a98477d761c4894449f4a2d
Carlos F. Martínez Sánchez <carlos.f.martinez.s+github@gmail.com>
2025-01-26 17:27:36 -0300

    layouts auth and default

---
+++ src/app/layouts/auth/auth.layout.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth',
+  standalone: true,
+  imports: [],
+  templateUrl: './auth.layout.html',
+  styleUrl: './auth.layout.css'
+})
+export class AuthLayout {
+
+}
---
+++ src/app/layouts/default/default.layout.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-default',
+  standalone: true,
+  imports: [],
+  templateUrl: './default.layout.html',
+  styleUrl: './default.layout.css'
+})
+export class DefaultLayout {
+
+}
