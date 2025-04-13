196755957cc7a9f0c0cfd6b323237672ae6ddf85
Carlos F. Martínez Sánchez <carlos.f.martinez.s+github@gmail.com>
2025-04-12 22:34:08 -0400

    version preliminar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,38 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./layouts/auth/auth.layout').then(m => m.AuthLayout)
+    redirectTo: 'auth',
+    pathMatch: 'full'
   },
+  {
+    path: 'auth',
+    loadComponent: () => import('./layouts/auth/auth.layout').then(m => m.AuthLayout),
+    children: [
+      {
+        path: '',
+        redirectTo: 'sign-in',
+        pathMatch: 'full'
+      },
+      {
+        path: 'sign-in',
+        loadComponent: () => import('./pages/auth/sign-in/sign-in.page').then(m => m.SignInPage)
+      },
+      {
+        path: 'sign-out',
+        loadComponent: () => import('./pages/auth/sign-out/sign-out.page').then(m => m.SignOutPage)
+      },
+      {
+        path: 'sign-up',
+        loadComponent: () => import('./pages/auth/sign-up/sign-up.page').then(m => m.SignUpPage)
+      },
+      {
+        path: 'recovery-password',
+        loadComponent: () => import('./pages/auth/recovery/recovery.page').then(m => m.RecoveryPage),
+      },
+      {
+        path: 'reset-password',
+        loadComponent: () => import('./pages/auth/reset-password/reset-password.page').then(m => m.ResetPasswordPage)
+      }
+    ]
+  }
 ];
